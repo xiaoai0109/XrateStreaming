1666ea83e35cc84aa89f2fb56acf22ae5645d8ea
xiaoai0109 <xiaoai0109@gmail.com>
2019-04-18 11:52:29 -0700

    Add getModelData method, Add README, update run_spark2.sh

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # XrateStreaming
+1. Clone this project into Cloudera
+2. Use "sbt", then type "eclipse", ensure all the packages are imported and runnable in Eclipse
+3. Use "sbt clean package" under this project, make sure the jar file "target/scala-2.11/xratestreaming\_2.11-0.1.0-SNAPSHOT.jar" is generated
+4. Run the script under this project, use "sh run\_spark2.sh" (without "\")
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -3,11 +3,15 @@ package example
 import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.spark.streaming.flume.{ FlumeUtils, SparkFlumeEvent }
 import org.apache.spark.streaming.{ Seconds, Minutes, StreamingContext }
-import org.apache.spark.sql.{ SQLContext, SparkSession }
-import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.{ SQLContext, SparkSession, DataFrame }
+import org.apache.spark.sql.functions.{ col, udf }
 import java.util.Arrays;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import java.util.{ Calendar, Date }
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.client._
@@ -15,32 +19,44 @@ import org.apache.hadoop.hbase.util.Bytes
 
 object Hello extends Greeting with App {
   println(greeting)
-  var i = 1
-  
-  val sparkSession = SparkSession.builder.master("local[3]")
-    .appName("Xrate Streaming").getOrCreate()
+
+  //  BasicConfigurator.configure()
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.WARN)
+
+  var sentimentModel: Double = 0.0
+  var latestModel: String = ""
+  getModelData()
+
+  var i = 1
+
+  val sparkSession = SparkSession.builder.master("local[3]")
+    .appName("Xrate Streaming").getOrCreate()
   val sc = sparkSession.sparkContext
   val sqlContext = sparkSession.sqlContext
-  val ssc = new StreamingContext(sc, Minutes(1)) 
+  val ssc = new StreamingContext(sc, Minutes(1))
   val flumeStream = FlumeUtils.createStream(ssc, "127.0.0.1", 9990)
   val win = flumeStream.window(Minutes(3), Minutes(1))
 
   import sparkSession.implicits._
 
   win.foreachRDD { rdd =>
-  println("\ncount: " + rdd.count())  // executed at the driver
-  rdd.foreach { record =>
-    val data = new String(record.event.getBody().array()) 
-    println(data)
-//    val data = """{"name":"Yin","address":{"city":"Columbus","state":"Ohio"}}"""
-//    println(df.select(col("address").getItem("state").as("state")).show())
-    val df = sqlContext.read.json(Seq(data).toDS)
-    println(df.select(col("RealtimeCurrencyExchangeRate").getItem("5.ExchangeRate").as("xrate")).show())
-//    df.printSchema()
+    println("\ncount: " + rdd.count()) // executed at the driver
+    if (latestModel != LocalDate.now.toString() &&
+      Calendar.getInstance().get(Calendar.HOUR_OF_DAY) == 10) {
+      println("here")
+      getModelData()
+    }
+    rdd.foreach { record =>
+      val data = new String(record.event.getBody().array())
+      println(data)
+      //    val data = """{"name":"Yin","address":{"city":"Columbus","state":"Ohio"}}"""
+      //    println(df.select(col("address").getItem("state").as("state")).show())
+      val df = sqlContext.read.json(Seq(data).toDS)
+      println(df.select(col("RealtimeCurrencyExchangeRate").getItem("5.ExchangeRate").as("xrate")).show())
+      //    df.printSchema()
+    }
   }
-}
 
   ssc.start()
   ssc.awaitTermination()
@@ -49,23 +65,43 @@ object Hello extends Greeting with App {
     println(xrate)
   }
 
-  private def getXrate(data: String): Double = {
-    try {
-      val array = data.split(",")
-      val xrate = array(1).toDouble
-      xrate
-    } catch {
-      case _: Throwable => 0
-    }
-  }
-
-  private def writeToHBase(xrate: Double) {
-    val hConf = new HBaseConfiguration()
+  private def writeToHBase(xrate: Double, df: DataFrame) {
+    //    val hConf = new HBaseConfiguration()
+    val hConf = HBaseConfiguration.create()
     val hTable = new HTable(hConf, "sk:test1")
     val thePut = new Put(i.toString.getBytes())
     thePut.addColumn("i".getBytes(), "a".getBytes(), xrate.toString.getBytes())
+    //    thePut.addColumn("i".getBytes(), "xrate".getBytes(), df.get(0).getBytes())
     hTable.put(thePut)
     i += 1
+
+  }
+
+  private def getModelData() {
+    //    val dateFormatter = new SimpleDateFormat("yyyy-MM-dd")
+    //    val key = dateFormatter.format(new Date())
+    val key = LocalDate.now().toString
+    val family = "sentiment"
+    val column = "model"
+    println(key)
+    try {
+      val hConf = HBaseConfiguration.create()
+      val hTable = new HTable(hConf, "model_data")
+      val theGet = new Get(key.getBytes())
+      val result = hTable.get(theGet)
+      val value = Bytes.toString(result.getValue(family.getBytes(), column.getBytes()))
+      println("key:" + value)
+      sentimentModel = value.toDouble
+      println("model:" + sentimentModel)
+
+      // TODO: Read Logistic Regression Model from Parquet
+
+      latestModel = key
+    } catch {
+      case e: Throwable =>
+        println(e.getMessage)
+        println("Cannot get sentiment model of " + key)
+    }
   }
 }
 
