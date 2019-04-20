6d347eccb1607c95c7b0bca56cdc7e503134b2c0
xiaoai0109 <xiaoai0109@gmail.com>
2019-04-20 03:50:22 -0700

    Create jar for mongoDB, export dump of Prediction from mongoDB

--- run_spark2.sh
+++ run_spark2.sh
@@ -1,5 +1,6 @@
 #sbt clean package
+#hdfs dfs -put myModelPath/ /user/cloudera/
 
-#spark2-submit --master local[2] --class example.Hello --packages org.apache.hbase:hbase-common:1.2.0,org.apache.spark:spark-streaming-flume_2.11:2.2.0,org.apache.hbase:hbase-client:1.2.0,org.apache.spark:spark-sql_2.12:2.2.0,org.apache.spark:spark-mllib_2.12:2.2.0 --name XrateStreaming target/scala-2.11/xratestreaming_2.11-0.1.0-SNAPSHOT.jar 
+#spark2-submit --master local[2] --class example.Hello --packages org.apache.hbase:hbase-common:1.2.0,org.apache.spark:spark-streaming-flume_2.11:2.2.0,org.apache.hbase:hbase-client:1.2.0,org.apache.spark:spark-sql_2.12:2.2.0,org.apache.spark:spark-mllib_2.12:2.2.0 --name XrateStreaming hbase_xratestreaming_2.11-0.1.0-SNAPSHOT.jar 
 
-spark2-submit --master local[2] --class example.Hello --packages org.apache.hbase:hbase-common:1.2.0,org.apache.spark:spark-streaming-flume_2.11:2.2.0,org.apache.hbase:hbase-client:1.2.0,org.apache.spark:spark-sql_2.12:2.2.0,org.apache.spark:spark-mllib_2.12:2.2.0 --name XrateStreaming xratestreaming_2.11-0.1.0-SNAPSHOT.jar 
+spark2-submit --master local[2] --class example.Hello --packages org.apache.hbase:hbase-common:1.2.0,org.apache.spark:spark-streaming-flume_2.11:2.2.0,org.apache.hbase:hbase-client:1.2.0,org.apache.spark:spark-sql_2.12:2.2.0,org.apache.spark:spark-mllib_2.12:2.2.0,org.mongodb.spark:mongo-spark-connector_2.11:2.4.0 --name XrateStreaming xratestreaming_2.11-0.1.0-SNAPSHOT.jar 
